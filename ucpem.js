97fa41326066cfcc570481769e7a4a4f872825a7
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-01-09 16:41:19 +0100

    UCPeM updated

--- ucpem.js
+++ ucpem.js
@@ -3,6 +3,11 @@
 const { project, github } = require("ucpem");
 
 project.prefix("src").res("drawer")
-project.prefix("src").res("drawerInput",
+
+const drawerInput = project.prefix("src").res("drawerInput",
     github("bt7s7k7/EventLib").res("eventLib")
+)
+
+project.prefix("src").res("drawerInputVue",
+    drawerInput
 )
