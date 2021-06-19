594499d8ffa2a4e07b0e8b28a5a0d1133c9efe7f
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-06-19 11:56:25 +0200

    Added DrawerView for Vue3

---
+++ src/drawerInputVue3/DrawerView.tsx
@@ -0,0 +1,67 @@
+import { defineComponent, onMounted, onUnmounted, PropType, ref, watch } from 'vue'
+import { Drawer } from '../drawer/Drawer'
+import { DrawerInput } from '../drawerInput/DrawerInput'
+import { DrawerInputConsumer } from '../drawerInput/DrawerInputConsumer'
+
+export const DrawerView = defineComponent({
+    name: "DrawerView",
+    props: {
+        drawerInput: {
+            type: DrawerInput,
+            default: () => new DrawerInput()
+        },
+        consumer: {
+            type: Object as PropType<DrawerInputConsumer.Builder>
+        }
+    },
+    setup(props, ctx) {
+        const canvas = ref<HTMLCanvasElement>(null!)
+        const drawer = ref<Drawer>()
+
+        onMounted(() => {
+            let rafId = 0
+            drawer.value = new Drawer(canvas.value.getContext("2d")!)
+
+            const update = () => {
+                rafId = requestAnimationFrame(update)
+
+                props.drawerInput?.processDrawEvent(drawer.value!, null)
+            }
+            update()
+
+            const keyDown = (event: KeyboardEvent) => props.drawerInput?.processKeyboardEvent(drawer.value!, "down", event)
+            const keyUp = (event: KeyboardEvent) => props.drawerInput?.processKeyboardEvent(drawer.value!, "up", event)
+
+            window.addEventListener("keydown", keyDown)
+            window.addEventListener("keyup", keyUp)
+
+            onUnmounted(() => {
+                cancelAnimationFrame(rafId)
+                window.removeEventListener("keydown", keyDown)
+                window.removeEventListener("keyup", keyUp)
+            })
+        })
+
+        let prevConsumer: DrawerInputConsumer | null = null
+        onUnmounted(() => prevConsumer?.dispose())
+
+        watch(() => props.consumer, (newConsumer) => {
+            prevConsumer?.dispose()
+
+            if (newConsumer) {
+                prevConsumer = newConsumer.create(props.drawerInput)
+            }
+        }, { immediate: true })
+
+        return () => (
+            <canvas
+                ref={canvas}
+                onMousemove={event => props.drawerInput?.processMouseInput(drawer.value!, "move", event)}
+                onMousedown={event => props.drawerInput?.processMouseInput(drawer.value!, "down", event)}
+                onMouseup={event => props.drawerInput?.processMouseInput(drawer.value!, "up", event)}
+                onMouseleave={event => props.drawerInput?.processMouseInput(drawer.value!, "leave", event)}
+                onContextmenu={event => event.preventDefault()}
+            ></canvas>
+        )
+    }
+})
--- ucpem.js
+++ ucpem.js
@@ -1,13 +1,18 @@
 /// <reference path="./.vscode/config.d.ts" />
 
-const { project, github } = require("ucpem");
+const { project, github } = require("ucpem")
 
-project.prefix("src").res("drawer")
+const drawer = project.prefix("src").res("drawer")
 
 const drawerInput = project.prefix("src").res("drawerInput",
+    drawer,
     github("bt7s7k7/EventLib").res("eventLib")
 )
 
 project.prefix("src").res("drawerInputVue",
+    drawerInput
+)
+
+project.prefix("src").res("drawerInputVue3",
     drawerInput
 )
