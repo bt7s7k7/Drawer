06a89f6c2cb731f866ce97a3084f7f6be06f7ef9
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-10-28 16:29:22 +0200

    Color conversion functions

--- src/drawer/Color.ts
+++ src/drawer/Color.ts
@@ -30,12 +30,37 @@ export class Color {
         return new Color(this.r, this.g, this.b, opacity)
     }
 
+    public toHex() {
+        return "#"
+            + Math.floor(this.r * 255).toString(16).padStart(2, "0")
+            + Math.floor(this.g * 255).toString(16).padStart(2, "0")
+            + Math.floor(this.b * 255).toString(16).padStart(2, "0")
+    }
+
+    public readonly r: number
+    public readonly g: number
+    public readonly b: number
+    public readonly a: number
+
+    constructor()
+    constructor(r: number, b: number, g: number, a?: number)
+    constructor(source: { r: number, b: number, g: number, a?: number })
     constructor(
-        public readonly r = 0,
-        public readonly g = 0,
-        public readonly b = 0,
-        public readonly a = 1
-    ) { }
+        r?: number | { r: number, b: number, g: number, a?: number },
+        g?: number, b?: number, a?: number
+    ) {
+        if (typeof r == "object") {
+            this.r = r.r
+            this.g = r.g
+            this.b = r.b
+            this.a = r.a ?? 1
+        } else {
+            this.r = r!
+            this.g = g!
+            this.b = b!
+            this.a = a ?? 1
+        }
+    }
 
     public static white = new Color(1, 1, 1)
     public static black = new Color(0, 0, 0)
@@ -54,6 +79,31 @@ export class Color {
         const r = parseInt(source.substr(offset + 0, 2), 16) / 255
         const g = parseInt(source.substr(offset + 2, 2), 16) / 255
         const b = parseInt(source.substr(offset + 4, 2), 16) / 255
+
+        return new Color(r, g, b)
+    }
+
+    public static fromHSL(h: number, s: number, l: number) {
+        var r, g, b
+
+        if (s == 0) {
+            r = g = b = l
+        } else {
+            const hue2rgb = (p: number, q: number, t: number) => {
+                if (t < 0) t += 1
+                if (t > 1) t -= 1
+                if (t < 1 / 6) return p + (q - p) * 6 * t
+                if (t < 1 / 2) return q
+                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+                return p
+            }
+
+            const q = l < 0.5 ? l * (1 + s) : l + s - l * s
+            const p = 2 * l - q
+            r = hue2rgb(p, q, h + 1 / 3)
+            g = hue2rgb(p, q, h)
+            b = hue2rgb(p, q, h - 1 / 3)
+        }
 
         return new Color(r, g, b)
     }
