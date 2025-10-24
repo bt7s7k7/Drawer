d5d59d3d1295999466ba6db345ec92884059ede9
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-10-24 20:04:05 +0200

    Page deploy

--- quick-draw/src/app/App.tsx
+++ quick-draw/src/app/App.tsx
@@ -1,13 +1,14 @@
 import { defineComponent } from "vue"
 import { DynamicsEmitter } from "../vue3gui/DynamicsEmitter"
+import { EditorPage } from "./EditorPage"
 
 export const App = defineComponent({
     name: "App",
     setup(props, ctx) {
         return () => (
             <DynamicsEmitter>
-                <router-view />
+                <EditorPage />
             </DynamicsEmitter>
         )
-    }
+    },
 })
--- quick-draw/vite.config.ts
+++ quick-draw/vite.config.ts
@@ -5,20 +5,24 @@ import { join } from "path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
     dotenv.config({ path: join(__dirname, ".env.local") })
     dotenv.config({ path: join(__dirname, ".env") })
 
     return {
         plugins: [vue(), vueJsx()],
         resolve: {
-            preserveSymlinks: true
+            preserveSymlinks: true,
         },
         server: {
             port: +(process.env.PORT ?? 8080),
             /* proxy: {
                 "^/api": { target: process.env.BACKEND_URL, changeOrigin: true },
             } */
-        }
+        },
+        build: {
+            outDir: "page",
+        },
+        base: mode == "development" ? "/" : process.env.BASE_URL,
     }
 })
